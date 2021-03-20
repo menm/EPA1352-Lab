0371d02ab3efd1465de5ef6352a6c73ce2784a1d
lidha <l.hu-6@student.tudelft.nl>
2021-03-20 13:14:15 +0100

    ran scenarios and push just to be sure

--- EPA1352-G14-A3/model/A3_model_run.py
+++ EPA1352-G14-A3/model/A3_model_run.py
@@ -10,7 +10,7 @@ from A3_model import BangladeshModel, driving_time
 seed = 12345670
 
 # Batch run the model, for the specified scenarios and number of iterations.
-parameter_sweep = {'seed': range(88888888, 88888890), 'scenario': range(4,5)}
+parameter_sweep = {'seed': range(88888890, 88888891), 'scenario': range(1,6)}
 
 num_iterations = 1
 num_steps = 5 * 24 * 60
@@ -31,4 +31,4 @@ df_batch = batch_run.get_model_vars_dataframe()
 print(df_batch)
 
 # Store the batchrunner to a csv
-df_batch.to_csv('../data/A3_scenario3.csv', index=False)
+df_batch.to_csv('../data/A3_scenarios_part2.csv', index=False)
